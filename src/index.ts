0f724377392c3afdf14c1e91ef562f950cd58b64
Anton Kosykh <kelin2025@yandex.ru>
2022-04-11 19:14:45 +0300

    feat: Added `$isShiftDown` etc. stores

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,13 @@ export const keyup = createEvent<KeyboardEvent>();
 export const keydown = createEvent<KeyboardEvent>();
 export const keypress = createEvent<KeyboardEvent>();
 
-if (typeof document !== 'undefined') {
-  document.addEventListener('keyup', evt => keyup(evt));
-  document.addEventListener('keydown', evt => keydown(evt));
-  document.addEventListener('keypress', evt => keypress(evt));
-}
+export const $isShiftDown = createStore(false);
+export const $isCtrlDown = createStore(false);
+export const $isAltDown = createStore(false);
+
+$isShiftDown.on([keyup, keydown], (prev, evt) => evt.shiftKey);
+$isCtrlDown.on([keyup, keydown], (prev, evt) => evt.ctrlKey);
+$isAltDown.on([keyup, keydown], (prev, evt) => evt.altKey);
 
 const keyEvents = {
   keyup,
@@ -44,6 +46,7 @@ interface hotkeyT {
 }
 
 /** Returns `Event` that gets triggered when a certain key pressed (or keyup/keydown events triggered) */
+// @ts-expect-error
 export const hotkey: hotkeyT = (...args) => {
   const normalizedParams =
     typeof args[0] === 'string' ? { key: args[0], type: args[1] } : args[0];
@@ -60,8 +63,15 @@ export const hotkey: hotkeyT = (...args) => {
   if (normalizedParams.target) {
     return sample({
       clock: keyTriggered,
+      // @ts-expect-error
       target: normalizedParams.target,
     });
   }
   return keyTriggered;
 };
+
+if (typeof document !== 'undefined') {
+  document.addEventListener('keyup', evt => keyup(evt));
+  document.addEventListener('keydown', evt => keydown(evt));
+  document.addEventListener('keypress', evt => keypress(evt));
+}
